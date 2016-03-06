e6d3da45cc0f581a38b0be1b599c133241c142ab
Ken Sanislo <ken@intherack.com>
2016-03-05 22:17:57 -0800

    fix whitespace

--- source/main.cpp
+++ source/main.cpp
@@ -37,7 +37,6 @@ bool onProgress(u64 pos, u64 size){
 	return !hid::pressed(hid::BUTTON_B);
 }
 
-
 Result http_getinfo(char *url, app::App *app){
 	Result ret=0;
 	u8* buf;
@@ -319,87 +318,64 @@ int doWebInstall (char *url){
 /*BRESENHAAM ALGORITHM FOR LINE DRAWING*/
 void bhm_line(void *fb,int x1,int y1,int x2,int y2,u32 c)
 {
- int x,y,dx,dy,dx1,dy1,px,py,xe,ye,i;
- dx=x2-x1;
- dy=y2-y1;
- dx1=fabs(dx);
- dy1=fabs(dy);
- px=2*dy1-dx1;
- py=2*dx1-dy1;
- if(dy1<=dx1)
- {
-  if(dx>=0)
-  {
-   x=x1;
-   y=y1;
-   xe=x2;
-  }
-  else
-  {
-   x=x2;
-   y=y2;
-   xe=x1;
-  }
-  putpixel(fb,x,y,c);
-  for(i=0;x<xe;i++)
-  {
-   x=x+1;
-   if(px<0)
-   {
-    px=px+2*dy1;
-   }
-   else
-   {
-    if((dx<0 && dy<0) || (dx>0 && dy>0))
-    {
-     y=y+1;
-    }
-    else
-    {
-     y=y-1;
-    }
-    px=px+2*(dy1-dx1);
-   }
-   putpixel(fb,x,y,c);
-  }
- }
- else
- {
-  if(dy>=0)
-  {
-   x=x1;
-   y=y1;
-   ye=y2;
-  }
-  else
-  {
-   x=x2;
-   y=y2;
-   ye=y1;
-  }
-  putpixel(fb,x,y,c);
-  for(i=0;y<ye;i++)
-  {
-   y=y+1;
-   if(py<=0)
-   {
-    py=py+2*dx1;
-   }
-   else
-   {
-    if((dx<0 && dy<0) || (dx>0 && dy>0))
-    {
-     x=x+1;
-    }
-    else
-    {
-     x=x-1;
-    }
-    py=py+2*(dx1-dy1);
-   }
-   putpixel(fb,x,y,c);
-  }
- }
+	int x,y,dx,dy,dx1,dy1,px,py,xe,ye,i;
+	dx=x2-x1;
+	dy=y2-y1;
+	dx1=fabs(dx);
+	dy1=fabs(dy);
+	px=2*dy1-dx1;
+	py=2*dx1-dy1;
+	if(dy1<=dx1){
+		if(dx>=0){
+			x=x1;
+			y=y1;
+			xe=x2;
+		} else {
+			x=x2;
+			y=y2;
+			xe=x1;
+		}
+		putpixel(fb,x,y,c);
+		for(i=0;x<xe;i++){
+			x=x+1;
+			if(px<0){
+				px=px+2*dy1;
+			} else {
+				if((dx<0 && dy<0) || (dx>0 && dy>0)){
+					y=y+1;
+				} else {
+					y=y-1;
+				}
+				px=px+2*(dy1-dx1);
+			}
+			putpixel(fb,x,y,c);
+		}
+	} else {
+		if(dy>=0){
+			x=x1;
+			y=y1;
+			ye=y2;
+		} else {
+			x=x2;
+			y=y2;
+			ye=y1;
+		}
+		putpixel(fb,x,y,c);
+		for(i=0;y<ye;i++){
+			y=y+1;
+			if(py<=0){
+				py=py+2*dx1;
+			} else {
+				if((dx<0 && dy<0) || (dx>0 && dy>0)){
+					x=x+1;
+				} else {
+					x=x-1;
+				}
+				py=py+2*(dx1-dy1);
+			}
+			putpixel(fb,x,y,c);
+		}
+	}
 }
 
 int main(int argc, char **argv){
